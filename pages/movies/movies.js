ed2c40dfc5a7337c3430164b1e4d6f705d2048a4
Juani Nicolai <78017476+juaninicolai@users.noreply.github.com>
2021-12-06 16:37:08 +0100

    Update movies.js

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -9,7 +9,7 @@ export default () => {
       const form = document.querySelector("form");
       form.addEventListener("submit", (event) => {
         event.preventDefault();
-        let movieUrl = "http://localhost:8080/api/movie";
+        let movieUrl = `${window.apiUrl}/api/movie`;
 
         fetch(movieUrl, {
           method: "POST",
